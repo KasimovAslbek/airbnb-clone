f7b32df38a08e354e3ce472ed45ed438a9364bda
KasimovAslbek <KasimovAslbek1994@mail.ru>
2023-08-19 12:18:14 +0500

    Login page has been created

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+        index page is here
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function LoginPage() {
+  return (
+    <div className='mt-4 grow flex items-center justify-around'>
+        <div className='mb-64'>
+            <h2 className='text-4xl text-center mb-4'>Login</h2>
+            <form className='max-w-md mx-auto '>
+                <input type="email" placeholder='your@email.com' name="" id="" />
+                <input type="password" name="" id="" placeholder='password' />
+                <button className='primary'>Login</button>
+                <div className='text-center py-2 text-gray-500'>Don't have an account yet? <Link to={"/register"} className='underline text-blue-700'>Register now</Link></div>
+            </form> 
+        </div>
+
+    </div>
+  )
+}
